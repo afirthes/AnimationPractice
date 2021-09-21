0bde03cb531258ee550acced994852296dc4b860
Afir Thes <afirthes@gmail.com>
2021-09-21 15:15:16 +0300

    Animate on tap

--- AnimationPractice/ViewController.swift
+++ AnimationPractice/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var block: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(blockTapped))
+        block.addGestureRecognizer(tap)
+        block.isUserInteractionEnabled = true
+        
     }
 
-
+    @objc func blockTapped() {
+        UIView.animate(withDuration: 1.2) {
+            let sizeMultiplier: CGFloat = 1.5
+            let currentBlockFrame = self.block.frame
+            
+            let newWidth = self.block.frame.width * sizeMultiplier
+            let newHeght = self.block.frame.height * sizeMultiplier
+            
+            let newX = self.block.frame.origin.x - (newWidth - self.block.frame.size.width) / 2
+            let newY = self.block.frame.origin.y - (newHeght - self.block.frame.size.height) / 2
+            
+            self.block.frame = CGRect(x: newX, y: newY, width: newWidth, height: newHeght)
+            
+        }
+    }
 }
 
