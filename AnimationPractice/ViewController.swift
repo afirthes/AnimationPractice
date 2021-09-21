9f00f0ecbfbc07ca2bb7c613c15199b305c09ebf
Afir Thes <afirthes@gmail.com>
2021-09-21 14:57:41 +0300

    Initial Commit

---
+++ AnimationPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationPractice
+//
+//  Created by Afir Thes on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
